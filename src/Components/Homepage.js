b8991b274485cb566e21f257936909665e7e1062
claykabongok <clayka68@gmail.com>
2020-08-16 11:50:47 +0200

    Initial commit

---
+++ src/Components/Homepage.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Country from "./Country";
+import axios from "axios";
+
+import loadingIcon from "../Assets/dashboardloader3.gif";
+
+
+
+export default function Homepage() {
+  const [loading, setLoading] = useState(true);
+  //const [error, setError] = useState(false);
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    //setError(false);
+
+    const api = "https://restcountries.eu/rest/v2/region/africa";
+    axios({
+      method: "GET",
+      url: `${api}`,
+    })
+      .then((res) => {
+        console.log(res.data);
+        setCountries(res.data);
+
+        setLoading(false);
+      })
+      .catch((e) => {
+        //setError(true);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      
+      <div>
+        {loading && (
+          <img src={loadingIcon} alt="loading" className="loadingIcon" />
+        )}
+      </div>
+      <div className="container-contries">
+      
+        <div class="row ">
+          {countries.map((data) => (
+            <Country data={data} key={data.numericCode} />
+          ))}
+        </div>
+      </div>
+  
+    </>
+  );
+}
