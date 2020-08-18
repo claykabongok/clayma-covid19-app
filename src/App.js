47a14ac1dcfbf33d013cd8ff2c2753285e24dd03
claykabongok <clayka68@gmail.com>
2020-08-18 08:44:34 +0200

    code splitting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { Suspense, lazy } from "react";
+import "./Styles/App.scss";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import './Styles/App.scss';
-import {
-  BrowserRouter as Router, 
-  Route,
-  
-  Switch,
-} from "react-router-dom";
+import loadingIcon from "./Assets/dashboardloader3.gif";
 
-import Homepage from "./Components/Homepage";
-
+const Homepage = lazy(() => import("./Components/Homepage"));
 
 function App() {
   return (
-    <Router  >
-    <Switch>
-      <Route  exact path="/" component={Homepage} />
-    
-    </Switch>
-  </Router>
+    <Router>
+      <Suspense
+        fallback={
+          <img src={loadingIcon} alt="loading" className="loadingIcon" />
+        }
+      >
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
